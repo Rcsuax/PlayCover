6aa5dfd73f3be343435612e0fb56be52cd517740
Alexandr <write2me4help@gmail.com>
2021-08-17 02:21:06 +0300

    * Dock now is hidden in Fullscreen mode

--- PlayCover/PlayCoverInject/MacHelper/NSWindowHelper.swift
+++ PlayCover/PlayCoverInject/MacHelper/NSWindowHelper.swift
@@ -9,7 +9,8 @@ import AppKit
 @objc public class NSWindowHelper : NSObject {
     
     @objc static public func initUI(){
-//        let window = NSApplication.shared.windows.first
+        let window = NSApplication.shared.windows.first
+        
 //        var frame = window?.frame
 //        if let screen = NSScreen.main {
 //            let rect = screen.frame
@@ -17,8 +18,23 @@ import AppKit
 //            let width = rect.size.width
 //            frame?.size = NSSize(width: (2560 * 1.3)  , height: (1440 * 1.3) )
 //        }
-//
-//        window?.setFrame(frame!, display: true)
-//        window?.center()
+        
+        let centre = NotificationCenter.default
+        let main = OperationQueue.main
+        
+        centre.addObserver(forName: NSWindow.willEnterFullScreenNotification, object: nil, queue: main) { (note) in
+            NSApplication.shared.presentationOptions = [NSApplication.PresentationOptions.hideMenuBar, NSApplication.PresentationOptions.hideDock]
+        }
+        
+        centre.addObserver(forName: NSWindow.willExitFullScreenNotification, object: nil, queue: main) { (note) in
+           NSApplication.shared.presentationOptions = []
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            window?.toggleFullScreen(true)
+        }
+        
+        //window?.setFrame(frame!, display: true)
+        //window?.center()
     }
 }
